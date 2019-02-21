37972a650c07fd206fff2a0fd8f302d9bcd064e7
ReBBiTpb <ReBBiTpb@gmail.com>
2019-02-21 15:44:39 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var obj = {};
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+console.log(persone.age);
+console.log(persone["name"]);
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[1]);
+// alert("Hello Word");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+// let answer = prompt("Есть ли вам 18 лет?", 'Да');
+// console.log(typeof(null));
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr); 
+console.log("2" === 2);
